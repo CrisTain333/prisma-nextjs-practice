09ce741d981e3e8b29d355d86395c2f4ee587cb2
Sukanta Das <arnobh84@gmail.com>
2023-10-02 18:38:11 +0600

    user card component created

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import { prisma } from "@/global/prisma";
 import CreateUser from "./components/CreateUser";
+import UserCard from "./components/UserCard";
 
 export default async function Home() {
   const users = await prisma.user.findMany();
@@ -15,11 +16,12 @@ export default async function Home() {
       <h2 className="text-5xl text-center mt-20">
         All Users
       </h2>
-      {users.map((user) => (
+      {/* {users.map((user) => (
         <p className="text-3xl my-3 font-semibold text-amber-400">
           Hello {user.id}.{user.name}
         </p>
-      ))}
+      ))} */}
+      <UserCard data={users} />
     </>
   );
 }
